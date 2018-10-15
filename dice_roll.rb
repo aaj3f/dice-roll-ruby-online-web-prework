ac4056021f6b525e23165374f2274d2965e596a4
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-15 14:45:08 +0000

    automatically backed up by learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,7 +3,6 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  dice = Array.new[1, 2, 3, 4, 5, 6]
-  binding.pry
+  dice = [1, 2, 3, 4, 5, 6]
   return dice[rand(0..5)]
 end
